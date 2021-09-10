e9b7dcf651a0c6f7c04ea85778b217c9fcef9235
Faisal Nadeem <mfaisalnadeem@hotmail.com>
2021-09-10 21:27:53 +0500

    default values for parseOptions

--- src/dashboard/Settings/GeneralSettings.react.js
+++ src/dashboard/Settings/GeneralSettings.react.js
@@ -51,6 +51,28 @@ const DEFAULT_SETTINGS_LABEL_WIDTH = 55;
 
 let numJobsFromRequestLimit = (limit) => Math.floor((limit-10)/20);
 
+const defaultParseOptions = {
+  accountLockout: {
+    duration: 5, // duration policy setting determines the number of minutes that a locked-out account remains locked out before automatically becoming unlocked. Set it to a value greater than 0 and less than 100000.
+    threshold: 3, // threshold policy setting determines the number of failed sign-in attempts that will cause a user account to be locked. Set it to an integer value greater than 0 and less than 1000.
+  },
+  // optional settings to enforce password policies
+  passwordPolicy: {
+    // Two optional settings to enforce strong passwords. Either one or both can be specified.
+    // If both are specified, both checks must pass to accept the password
+    // 1. a RegExp object or a regex string representing the pattern to enforce
+    validatorPattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/, // enforce password with at least 8 char with at least 1 lower case, 1 upper case and 1 digit
+    // 2. a callback function to be invoked to validate the password
+    validatorCallback: '(password) => { return validatePassword(password) }',
+    validationError: 'Password must contain at least 1 digit.', // optional error message to be sent instead of the default "Password does not meet the Password Policy requirements." message.
+    doNotAllowUsername: true, // optional setting to disallow username in passwords
+    maxPasswordAge: 90, // optional setting in days for password expiry. Login fails if user does not reset the password within this period after signup/last reset.
+    maxPasswordHistory: 5, // optional setting to prevent reuse of previous n passwords. Maximum value that can be specified is 20. Not specifying it or specifying 0 will not enforce history.
+    //optional setting to set a validity duration for password reset links (in seconds)
+    resetTokenValidityDuration: 24*60*60, // expire after 24 hours
+  }
+}
+
 let CurrentPlan = ({requestLimit}) => {
   let costString = requestLimit === 30 ?
     'Free' :
@@ -238,9 +260,11 @@ let ManageAppFields = ({
       };
     }
     else if ( parseOptions instanceof Object ) {
-      parseOptionsJson = parseOptions;
+      parseOptionsJson = { accountLockout: { ...parseOptions.accountLockout }, passwordPolicy: { ...parseOptions.passwordPolicy } };
     }
   }
+
+  console.log(parseOptions);
 
   return (
     <Fieldset
@@ -264,8 +288,6 @@ let ManageAppFields = ({
                 min={0}
                 defaultValue={getSettingsFromKey(parseOptionsJson.passwordPolicy, 'resetTokenValidityDuration') || 24*60*60}
                 onChange={resetTokenValidityDuration => {
-                  if(validateSettingsFloatMinMax(resetTokenValidityDuration, 0, 1000) === false)
-                    return;
                   parseOptionsJson.passwordPolicy.resetTokenValidityDuration = resetTokenValidityDuration;
                   setParseOptions(JSON.stringify( parseOptionsJson ));
                 }} />
@@ -747,7 +769,9 @@ export default class GeneralSettings extends DashboardView {
           }
           if (changes.appName !== undefined || changes.parseOptions !== undefined ) {
             const parseOptions = {...typeof changes.parseOptions == 'string' ? JSON.parse(changes.parseOptions) : {} };
-            promiseList.push(this.context.currentApp.setAppConfig(changes.appName, parseOptions));
+            promiseList.push(this.context.currentApp.setAppConfig(changes.appName,
+              { accountLockout: {...defaultParseOptions.accountLockout, ...parseOptions.accountLockout}, passwordPolicy: { ...defaultParseOptions.passwordPolicy, ...parseOptions.passwordPolicy }}
+            ));
           }
           if (changes.inProduction !== undefined) {
             promiseList.push(this.context.currentApp.setInProduction(changes.inProduction));
@@ -783,13 +807,12 @@ export default class GeneralSettings extends DashboardView {
               promiseList.push(this.context.currentApp.setAppStoreURL(urlKeys[key], changes[key]));
             }
           });
-
-          return Promise.all(promiseList).then(() => {
-            this.forceUpdate(); //Need to forceUpdate to see changes applied to source ParseApp
-            this.setState({ removedCollaborators: removedCollaborators || [] });
-          }).catch(errors => {
-            return Promise.reject({ error: unique(pluck(errors, 'error')).join(' ')});
-          });
+          // return Promise.all(promiseList).then(() => {
+          //   this.forceUpdate(); //Need to forceUpdate to see changes applied to source ParseApp
+          //   this.setState({ removedCollaborators: removedCollaborators || [] });
+          // }).catch(errors => {
+          //   return Promise.reject({ error: unique(pluck(errors, 'error')).join(' ')});
+          // });
         }}
         renderForm={({ fields, setField }) => {
           //let isCollaborator = AccountManager.currentUser().email !== this.props.initialFields.owner_email;
