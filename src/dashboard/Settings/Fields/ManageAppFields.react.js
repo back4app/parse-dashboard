d8be69dee4798b289f6b60256ae94e9e1268f375
charles-ramos <charles.ramos@fatec.sp.gov.br>
2022-09-09 18:16:00 -0300

    improving database URL

--- src/dashboard/Settings/Fields/ManageAppFields.react.js
+++ src/dashboard/Settings/Fields/ManageAppFields.react.js
@@ -89,7 +89,7 @@ export const ManageAppFields = ({
             labelWidth={'50%'}
             label={<LabelSettings
               text='Database Version'
-              description={<span>{databaseURL?.split('://')[0]} {databaseVersion ? databaseVersion : mongoVersion}</span>}
+              description={<span>{(databaseURL?.split('://')[0]).split('+srv')} {databaseVersion ? databaseVersion : mongoVersion}</span>}
             />}
           />
           </div>
