d785beb918490dc03c7a44badfc1c2edded00d1f
charles-ramos <charles.ramos@fatec.sp.gov.br>
2024-01-11 17:26:31 -0300

    testing clone

--- src/lib/ParseApp.js
+++ src/lib/ParseApp.js
@@ -440,7 +440,8 @@ export default class ParseApp {
 
   createApp(appName, parseVersion, originalAppId = null) {
     const path = '/parse-app';
-    return AJAX.post(path, { appDescription: '', parseVersion, originalAppId: originalAppId, appId: null, appName, isPublic: false })
+    
+    return axios.post(path, { appDescription: '', parseVersion, originalAppId: originalAppId, appId: null, appName, isPublic: false }, { timeout: 120000} )
   }
 
   initializeDb(appId, parseVersion) {
