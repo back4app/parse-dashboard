1be646f2b8349edf1268bd77e617d1a68f1fc137
sadakchap <aliceprerna@gmail.com>
2024-03-27 21:34:40 +0530

    fix: app name tooltip position

--- src/components/Sidebar/AppName.react.js
+++ src/components/Sidebar/AppName.react.js
@@ -8,7 +8,7 @@ const AppName = ({ name, onClick, onPinClick }) => (
   <div>
     <div className={styles.currentApp}>
       <div className={styles.appNameContainer} onClick={onClick}>
-        <Tooltip className={styles.Tooltip} value={(<>{name}</>)}>
+        <Tooltip className={styles.Tooltip} value={(<span>{name}</span>)}>
           <div className={styles.currentAppName}>{name}</div>
         </Tooltip>
         <Icon name="b4a-chevron-down" width={18} height={18} />
