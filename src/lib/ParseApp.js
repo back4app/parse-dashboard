da4b8d22876149e303ca7a49410bdee84f75680e
charles-ramos <charles.ramos@fatec.sp.gov.br>
2024-07-29 12:48:39 -0300

    hide connection string - permissions

--- src/lib/ParseApp.js
+++ src/lib/ParseApp.js
@@ -55,7 +55,6 @@ export default class ParseApp {
     graphQLServerURL,
     columnPreference,
     databaseURL,
-    hideDatabaseURL,
     permissions,
     scripts,
     classPreference,
@@ -91,7 +90,7 @@ export default class ParseApp {
     this.graphQLServerURL = graphQLServerURL;
     this.columnPreference = columnPreference;
     this.databaseURL = databaseURL;
-    this.hideDatabaseURL = hideDatabaseURL;
+    this.permissions = permissions;
     this.scripts = scripts;
     this.enableSecurityChecks = !!enableSecurityChecks;
     this.useLatestDashboardVersion = useLatestDashboardVersion !== false;
