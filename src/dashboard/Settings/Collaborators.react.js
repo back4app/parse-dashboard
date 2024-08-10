fd9d5986f3c219f037b13f2652b7a18480d84c3e
charles-ramos <charles.ramos@fatec.sp.gov.br>
2024-08-10 14:36:53 -0300

    maxCollaborators - Limit

--- src/dashboard/Settings/Collaborators.react.js
+++ src/dashboard/Settings/Collaborators.react.js
@@ -335,10 +335,8 @@ export default class Collaborators extends React.Component {
   }
 
   addCollaboratorField() {
-    const { permissions, collaborators, waiting_collaborators } = this.props;
-    const totalCollaborators = collaborators.length + waiting_collaborators.length;
-    const maxCollaborators = permissions ? permissions.maxCollaborators : 0;
-    const limitReached = maxCollaborators > 0 && totalCollaborators >= maxCollaborators;
+    const limitReached = this.props.permissions &&
+      (this.props.collaborators.length + this.props.waiting_collaborators.length) >= this.props.permissions.maxCollaborators;
   
     return (
       <Field
@@ -350,32 +348,40 @@ export default class Collaborators extends React.Component {
           />
         }
         input={
-          <InlineSubmitInput
-            render={() => {
-              return (
-                <TextInput
-                  placeholder={limitReached ? 
-                    "Limit reached. Add more spots on the pricing page." :
-                    "What&#39;s their email?"}
-                  value={this.state.currentEmail}
-                  onChange={(value) => {
-                    this.setState({ currentEmail: value, showBtnCollaborator: this.validateEmail(value) });
-                  }}
-                  disabled={limitReached} 
-                />
-              );
-            }}
-            showButton={this.state.showBtnCollaborator}
-            validate={(email) => {
-              return this.validateEmail(email) || this.state.showBtnCollaborator;
-            }}
-            onSubmit={this.handleAdd.bind(this)}
-            submitButtonText='ADD'
-          />
+          limitReached ? (
+            <a href="https://www.back4app.com/pricing/backend-as-a-service" target="_blank" rel="noopener noreferrer">
+              Add More Spots
+            </a>
+          ) : (
+            <InlineSubmitInput
+              render={() => {
+                return (
+                  <TextInput
+                    placeholder="What&#39;s their email?"
+                    value={this.state.currentEmail}
+                    onChange={(value) => {
+                      this.setState({ currentEmail: value, showBtnCollaborator: this.validateEmail(value) });
+                    }}
+                    disabled={false} 
+                  />
+                );
+              }}
+              showButton={this.state.showBtnCollaborator}
+              validate={(email) => {
+                if (this.state.showBtnCollaborator === true) {
+                  return true;
+                }
+                return this.validateEmail(email);
+              }}
+              onSubmit={this.handleAdd.bind(this)}
+              submitButtonText='ADD'
+            />
+          )
         }
       />
     );
   }
+  
 
   listAppOwnerEmail() {
     return (
