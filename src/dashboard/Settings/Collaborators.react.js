f8d64ec5f4229c180c436773c26be29f5830f461
charles-ramos <charles.ramos@fatec.sp.gov.br>
2024-08-23 13:40:43 -0300

    maxCollaborators - Limit

--- src/dashboard/Settings/Collaborators.react.js
+++ src/dashboard/Settings/Collaborators.react.js
@@ -80,60 +80,27 @@ export default class Collaborators extends React.Component {
       lodash.mapValues(this.context.classCounts.counts, () => 'Write')
   }
 
-  // handleAdd() {
-  //   //TODO: Show some in-progress thing while the collaborator is being validated, or maybe have some sort of
-  //   //async validator in the parent form. Currently if you mash the add button, they same collaborator gets added many times.
-  //   this.setState({ lastError: '', lastSuccess: '', showBtnCollaborator: false });
-  //   return this.context.validateCollaborator(this.state.currentEmail).then((response) => {
-  //     // lastError logic assumes we only have 1 input field
-  //     if (response.success) {
-  //       this.setState({
-  //         showDialog: true,
-  //         toAdd: true,
-  //         lastError: '',
-  //         inviteCollab: false,
-  //         limitReached: prevState => ({prevState.limitReached + 1})
-  //       });
-  //       return true;
-  //     } else if (response.error) {
-  //       this.setState({ lastError: response.error });
-  //       return false;
-  //     }
-  //   }).catch(error => {
-  //     this.setState({ lastError: error.message, inviteCollab: error.status === 404 && true })
-  //   });
-  // }
-
-
   handleAdd() {
-    // Clear previous error messages and button state
+    //TODO: Show some in-progress thing while the collaborator is being validated, or maybe have some sort of
+    //async validator in the parent form. Currently if you mash the add button, they same collaborator gets added many times.
     this.setState({ lastError: '', lastSuccess: '', showBtnCollaborator: false });
-  
     return this.context.validateCollaborator(this.state.currentEmail).then((response) => {
+      // lastError logic assumes we only have 1 input field
       if (response.success) {
-        // Update the state and log the previous and new values of limitReached
-        this.setState(prevState => {
-          const newLimitReached = prevState.limitReached + 1;
-          console.log('Previous limitReached:', prevState.limitReached);
-          console.log('New limitReached:', newLimitReached);
-          return {
-            showDialog: true,
-            toAdd: true,
-            lastError: '',
-            inviteCollab: false,
-            limitReached: newLimitReached 
-          };
-        }, () => {
-          console.log('State after update:', this.state.limitReached);
+        this.setState({
+          showDialog: true,
+          toAdd: true,
+          lastError: '',
+          inviteCollab: false,
+          limitReached: prevState => ({prevState.limitReached + 1})
         });
-  
         return true;
       } else if (response.error) {
         this.setState({ lastError: response.error });
         return false;
       }
     }).catch(error => {
-      this.setState({ lastError: error.message, inviteCollab: error.status === 404 && true });
+      this.setState({ lastError: error.message, inviteCollab: error.status === 404 && true })
     });
   }
   
