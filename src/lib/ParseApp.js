33e4eda05184fa68b78a89f3eb50201931d412f9
charles-ramos <charles.ramos@fatec.sp.gov.br>
2024-09-01 19:06:08 -0300

    updating dashboard URL

--- src/lib/ParseApp.js
+++ src/lib/ParseApp.js
@@ -780,21 +780,6 @@ export default class ParseApp {
     return promise;
   }
 
-  addCollaborator(email, featuresPermission, classesPermission) {
-    const path = '/apps/' + this.slug + '/collaborations';
-    const promise = axios.post(path, {'collaboration[email]': email, featuresPermission, classesPermission});
-    promise.then(({ data }) => {
-      //TODO: this currently works because everything that uses collaborators
-      // happens to re-render after this call anyway, but really the collaborators
-      // should be updated properly in a store or AppsManager or something
-      this.settings.fields.fields.collaborators =
-        Array.isArray(this.settings.fields.fields.collaborators) ?
-          this.settings.fields.fields.collaborators : [];
-      this.settings.fields.fields.collaborators = [ ...this.settings.fields.fields.collaborators, data.data ];
-    });
-    return promise;
-  }
-
   setRequestLimit(limit) {
     const path = '/plans/' + this.slug + '?new_limit=' + limit.toString();
     const promise = AJAX.put(path);
