98f2831933fc3d974971bac788e605a90e7a1967
charles-ramos <charles.ramos@fatec.sp.gov.br>
2024-10-02 16:12:01 -0300

    maxCollaborators - limit

--- src/dashboard/Settings/Collaborators.react.js
+++ src/dashboard/Settings/Collaborators.react.js
@@ -94,7 +94,7 @@ export default class Collaborators extends React.Component {
           toAdd: true,
           lastError: '',
           inviteCollab: false,
-          collaboratorUsage: response.collaboratorUsage       //   collaboratorUsage: this.context.settings.fields.fields.collaboratorUsage ?? 0
+          collaboratorUsage: this.context.settings.fields.fields.collaboratorUsage
         });
         console.log('handle add')
         console.log(this.state.collaboratorUsage)
