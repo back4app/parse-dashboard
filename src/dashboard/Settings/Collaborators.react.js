9fcfac4d40e7833ebbe2c5d0becf6ac321c1337a
charles-ramos <charles.ramos@fatec.sp.gov.br>
2024-10-11 14:59:32 -0300

    MaxCollaborators - limit

--- src/dashboard/Settings/Collaborators.react.js
+++ src/dashboard/Settings/Collaborators.react.js
@@ -492,9 +492,8 @@ export default class Collaborators extends React.Component {
   render() {
     //working
     const maxCollaborators = this.context.settings.fields.fields.maxCollaborators;
-    const collaboratorUsage = (this.props.collaborators.length + this.props.waiting_collaborators <= (this.context.settings.fields.fields.collaboratorUsage ?? 0)) ? this.props.collaborators.length : (this.context.settings.fields.fields.collaboratorUsage ?? 0);
+    const collaboratorUsage = (this.props.collaborators.length + this.props.waiting_collaborators.length <= (this.context.settings.fields.fields.collaboratorUsage ?? 0)) ? (this.props.collaborators.length + this.props.waiting_collaborators.length) : (this.context.settings.fields.fields.collaboratorUsage ?? 0);
 
-    // const collaboratorUsage = (this.context.settings.fields.fields.collaboratorUsage ?? 0)
     console.log('context')
     console.log(this.context.settings)
     console.log('settings')
