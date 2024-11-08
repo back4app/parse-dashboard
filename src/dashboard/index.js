f2efa4612cda42c3b440fd7ff9f7d414a961162c
sadakchap <aliceprerna@gmail.com>
2024-11-08 15:27:48 +0530

    install sentry

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -12,6 +12,22 @@ import installDevTools from 'immutable-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './Dashboard';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: `${b4aSettings.BACK4APP_SENTRY_DSN_URL}`,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [/^https:\/\/back4app.com\/me/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 require('stylesheets/fonts.scss');
 require('graphiql/graphiql.min.css');
