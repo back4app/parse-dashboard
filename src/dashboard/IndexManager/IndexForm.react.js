14d440861c5e42026f6d33a3534cb682811c7308
Natália Condê <32825972+nataliaconde@users.noreply.github.com>
2025-02-26 11:53:24 -0300

    Update IndexForm.react.js

--- src/dashboard/IndexManager/IndexForm.react.js
+++ src/dashboard/IndexManager/IndexForm.react.js
@@ -221,15 +221,15 @@ class IndexForm extends Component {
       sparse,
       unique,
       // expireAfterSeconds,
-      // weights: Object.entries(weights).reduce((acc, [key, value]) => {
-      //   if (typeof value === 'string') {
-      //     acc[key] = parseInt(value)
-      //   } else {
-      //     acc[key] = value
-      //   }
-      //   return acc
-      // }, {})
     }
+
+    if (weights && Object.keys(weights).length > 0) {
+        indexOptions.weights = Object.entries(weights).reduce((acc, [key, value]) => {
+          acc[key] = typeof value === 'string' ? parseInt(value, 10) : value;
+          return acc;
+        }, {});
+      }
+
     const indexConfiguration = {
       index,
       indexOptions
