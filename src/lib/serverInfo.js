ee6a7b812122154dfdf87811e44350d72c25b9e0
sadakchap <aliceprerna@gmail.com>
2025-04-03 21:49:34 +0530

    allow settings

--- src/lib/serverInfo.js
+++ src/lib/serverInfo.js
@@ -1,4 +1,4 @@
-export const ALWAYS_ALLOWED_ROUTES = ['overview', 'web-deployment', 'Overview', 'Web Deployment'];
+export const ALWAYS_ALLOWED_ROUTES = ['overview', 'web-deployment', 'Overview', 'Web Deployment', 'settings', 'App Settings', 'server-settings'];
 
 export const canAccess = (serverInfo, route) => {
   if (ALWAYS_ALLOWED_ROUTES.includes(route)) {
