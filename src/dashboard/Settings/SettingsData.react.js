df3fbdb5d3444f1b21801d4b065fdbcc41e43f2e
sadakchap <aliceprerna@gmail.com>
2025-05-12 18:40:26 +0530

    make api call when app changes

--- src/dashboard/Settings/SettingsData.react.js
+++ src/dashboard/Settings/SettingsData.react.js
@@ -31,17 +31,19 @@ export default class SettingsData extends React.Component {
     if (this.context !== nextContext) {
       // check if the changes are in currentApp serverInfo status
       // if not return without making any request
-      if (this.props.apps !== nextProps.apps) {
-        const updatedCurrentApp = nextProps.apps.find(ap => ap.slug === this.props.params.appId);
-        const prevCurrentApp = this.props.apps.find(ap => ap.slug === this.props.params.appId);
-        const shouldUpdate = updatedCurrentApp.serverInfo.status !== prevCurrentApp.serverInfo.status;
-        if (!shouldUpdate) {return;}
+      // if (this.props.apps !== nextProps.apps) {
+      //   const updatedCurrentApp = nextProps.apps.find(ap => ap.slug === this.props.params.appId);
+      //   const prevCurrentApp = this.props.apps.find(ap => ap.slug === this.props.params.appId);
+      //   const shouldUpdate = updatedCurrentApp.serverInfo.status !== prevCurrentApp.serverInfo.status;
+      //   if (!shouldUpdate) {return;}
+      // }
+      if (this.context.applicationId !== nextContext.applicationId) {
+        console.log('received props settings API CALLED!')
+        this.setState({ fields: undefined });
+        nextContext.fetchSettingsFields().then(({ fields }) => {
+          this.setState({ fields });
+        });
       }
-      console.log('received props settings API CALLED!')
-      this.setState({ fields: undefined });
-      nextContext.fetchSettingsFields().then(({ fields }) => {
-        this.setState({ fields });
-      });
     }
   }
 
