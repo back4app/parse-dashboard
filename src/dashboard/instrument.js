a1abb88c20ec91a8aaad4c092d19230cfa922b82
sadakchap <aliceprerna@gmail.com>
2025-06-10 18:08:37 +0530

    some random change

--- src/dashboard/instrument.js
+++ src/dashboard/instrument.js
@@ -13,7 +13,7 @@ console.log('process.env.SENTRY_ENV', process.env.SENTRY_ENV);
 console.log('process.env.SENTRY_RECORD_X_HOURS', process.env.SENTRY_RECORD_X_HOURS);
 
 // const isLessThan2Hours = new Date(process.env.BUILD_TIMESTAMP) > new Date(Date.now() - 1000 * 60 * 60 * b4aSettings.SENTRY_RECORD_X_HOURS);
-const isLessThan2Hours = (Date.now() - new Date(process.env.BUILD_TIMESTAMP)) < (1000 * 60 * 2);
+const isLessThan2Hours = (Date.now() - new Date(process.env.BUILD_TIMESTAMP)) < (1000 * 60 * 3);
 const isRecordEverySession = (process.env.SENTRY_ENV === 'production' || process.env.SENTRY_ENV === 'homolog') && isLessThan2Hours;
 
 console.log('isLessThan2Hours', isLessThan2Hours);
