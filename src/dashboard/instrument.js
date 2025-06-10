fa0838223c1781d93b97526eff4033428759af81
sadakchap <aliceprerna@gmail.com>
2025-06-10 18:14:46 +0530

    fix console.log

--- src/dashboard/instrument.js
+++ src/dashboard/instrument.js
@@ -10,7 +10,7 @@ import {
 
 console.log('process.env.BUILD_TIMESTAMP', process.env.BUILD_TIMESTAMP);
 console.log('process.env.SENTRY_ENV', process.env.SENTRY_ENV);
-console.log('process.env.SENTRY_RECORD_X_HOURS', process.env.SENTRY_RECORD_X_HOURS);
+console.log('process.env.SENTRY_RECORD_X_HOURS', b4aSettings.SENTRY_RECORD_X_HOURS);
 
 // const isLessThan2Hours = new Date(process.env.BUILD_TIMESTAMP) > new Date(Date.now() - 1000 * 60 * 60 * b4aSettings.SENTRY_RECORD_X_HOURS);
 const isLessThan2Hours = (Date.now() - new Date(process.env.BUILD_TIMESTAMP)) < (1000 * 60 * 3);
