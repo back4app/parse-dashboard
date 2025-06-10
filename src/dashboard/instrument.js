07389a762c933f191a7c3fbf529690bd545a53be
sadakchap <aliceprerna@gmail.com>
2025-06-10 18:17:48 +0530

    use env hours

--- src/dashboard/instrument.js
+++ src/dashboard/instrument.js
@@ -7,14 +7,10 @@ import {
   useNavigationType,
 } from 'react-router-dom';
 
-
-console.log('process.env.BUILD_TIMESTAMP', process.env.BUILD_TIMESTAMP);
-console.log('process.env.SENTRY_ENV', process.env.SENTRY_ENV);
-console.log('process.env.SENTRY_RECORD_X_HOURS', b4aSettings.SENTRY_RECORD_X_HOURS);
-
-// const isLessThan2Hours = new Date(process.env.BUILD_TIMESTAMP) > new Date(Date.now() - 1000 * 60 * 60 * b4aSettings.SENTRY_RECORD_X_HOURS);
-const isLessThan2Hours = (Date.now() - new Date(process.env.BUILD_TIMESTAMP)) < (1000 * 60 * 3);
+// eslint-disable-next-line no-undef
+const isLessThan2Hours = (Date.now() - new Date(process.env.BUILD_TIMESTAMP)) < (1000 * 60 * 60 * b4aSettings.SENTRY_RECORD_X_HOURS);
 const isRecordEverySession = (process.env.SENTRY_ENV === 'production' || process.env.SENTRY_ENV === 'homolog') && isLessThan2Hours;
+
 
 console.log('isLessThan2Hours', isLessThan2Hours);
 console.log('isRecordEverySession', isRecordEverySession);
