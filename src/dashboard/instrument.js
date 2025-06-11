38c485c9dc57b2b372795ce31ec549650175f000
sadakchap <aliceprerna@gmail.com>
2025-06-11 15:46:58 +0530

    start recording when user navigates to deployments page

--- src/dashboard/instrument.js
+++ src/dashboard/instrument.js
@@ -8,17 +8,26 @@ import {
 } from 'react-router-dom';
 
 // eslint-disable-next-line no-undef
-const isLessThan2Hours = (Date.now() - new Date(process.env.BUILD_TIMESTAMP)) < (1000 * 60 * 60 * (b4aSettings.SENTRY_RECORD_X_HOURS || 2));
+const isLessThan2Hours = (Date.now() - new Date(process.env.BUILD_TIMESTAMP)) < (1000 * 60 * 5);
 const isRecordEverySession = (process.env.SENTRY_ENV === 'production' || process.env.SENTRY_ENV === 'homolog') && isLessThan2Hours;
 
-console.log('isRecordEverySession', isRecordEverySession);
+const replay = Sentry.replayIntegration({
+  stickySession: true,
+  maskAllText: false,
+  blockAllMedia: true,
+  minReplayDuration: 5000,
+  maskAllInputs: false,
+  networkDetailAllowUrls: [/(https?:\/\/(.+?\.)?back4app\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/g],
+  networkRequestHeaders: ['X-Custom-Header'],
+  networkResponseHeaders: ['X-Custom-Header'],
+})
 
 Sentry.init({
   debug: process.env.NODE_ENV !== 'production',
   dsn: b4aSettings.SENTRY_DSN,
   environment: process.env.SENTRY_ENV,
   tracesSampleRate: 1.0,
-  replaysSessionSampleRate: isRecordEverySession ? 1.0 : 0.1,
+  replaysSessionSampleRate: 0,
   replaysOnErrorSampleRate: 1.0,
   maxBreadcrumbs: 100,
   integrations: [
@@ -29,19 +38,23 @@ Sentry.init({
       createRoutesFromChildren,
       matchRoutes,
     }),
-    Sentry.replayIntegration({
-      stickySession: true,
-      maskAllText: false,
-      blockAllMedia: true,
-      minReplayDuration: 5000,
-      maskAllInputs: false,
-      networkDetailAllowUrls: [/(https?:\/\/(.+?\.)?back4app\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/g],
-      networkRequestHeaders: ['X-Custom-Header'],
-      networkResponseHeaders: ['X-Custom-Header'],
-    }),
+    replay,
     Sentry.captureConsoleIntegration({ levels: ['error']}),
     Sentry.browserTracingIntegration(),
   ],
+});
+
+window.addEventListener('navigate', (event) => {
+  const url = new URL(event.destination.url);
+
+  let isDeploymentPath = false;
+  isDeploymentPath = url.pathname.split('/').includes('deployments');
+  console.log('isDeploymentPath', isDeploymentPath);
+  console.log('isRecordEverySession', isRecordEverySession);
+
+  if (isDeploymentPath && isRecordEverySession) {
+    replay.start();
+  }
 });
 
 
