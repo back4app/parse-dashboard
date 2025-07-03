7797f5c8ea826fc0a8b0922f3e1fef4acdc9a849
sadakchap <aliceprerna@gmail.com>
2025-07-03 20:21:26 +0530

    move up

--- src/dashboard/Data/AppOverview/AppOverview.react.js
+++ src/dashboard/Data/AppOverview/AppOverview.react.js
@@ -244,9 +244,10 @@ class AppOverview extends DashboardView {
             </div>
           </div>
 
+          <ComplianceCard loading={this.state.isLoadingAppPlanData} planData={this.state.appPlanData} appId={this.context.applicationId} isSignedBAA={this.context.custom.isSignedBAA} />
+
           <OnboardingBoxes slug={this.context.slug} appName={this.context.name} appId={this.context.applicationId} openConnectModal={() => this.setState({ showConnectAppModal: true })} />
 
-          <ComplianceCard loading={this.state.isLoadingAppPlanData} planData={this.state.appPlanData} appId={this.context.applicationId} isSignedBAA={this.context.custom.isSignedBAA} />
           {/* System Logs Card */}
           <SystemLogsCard loading={this.state.isLoadingServerLogs} logs={this.state.serverLogs} appSlug={this.context.slug} />
 
