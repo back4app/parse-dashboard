ac72977212450732e76efd342a927d6ecb69dc0e
sadakchap <aliceprerna@gmail.com>
2025-07-08 19:11:16 +0530

    remove app from list

--- src/dashboard/Settings/GeneralSettings.react.js
+++ src/dashboard/Settings/GeneralSettings.react.js
@@ -6,6 +6,7 @@
  * the root directory of this source tree.
  */
 import AccountManager from 'lib/AccountManager';
+import AppsManager from 'lib/AppsManager';
 import DashboardView from 'dashboard/DashboardView.react';
 import FlowView from 'components/FlowView/FlowView.react';
 import React from 'react';
@@ -103,6 +104,7 @@ export default class GeneralSettings extends DashboardView {
     this.setState({ removedCollaborators: removedCollaborators || [] });
 
     if (removedCollaborators.filter(collab => collab.userEmail === AccountManager.currentUser().email).length > 0) {
+      AppsManager.removeAppFromList(this.context.slug);
       this.props.navigate('/apps');
     }
   }
