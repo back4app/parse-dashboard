fe26136cbc24748df8ec4334814e6688ccd7f668
sadakchap <aliceprerna@gmail.com>
2025-07-23 17:55:10 +0530

    fix typo

--- src/dashboard/DashboardView.react.js
+++ src/dashboard/DashboardView.react.js
@@ -346,7 +346,7 @@ export default class DashboardView extends React.Component {
 
     advisorsSubSections.push({
       name: 'Slow Requests',
-      link: '/analytics/slow-requests',
+      link: '/analytics/slow_requests',
       // subsections: analyticsSubSections
     })
 
