14bb78b0c2485131a827ab31d77e98377e636fa8
Silas Rafael Barreto Prado <silas@MacBook-Air-de-Silas.local>
2026-02-19 09:43:56 -0300

    hidden change parse version button for collaborators

--- src/dashboard/Settings/Fields/ManageAppFields.react.js
+++ src/dashboard/Settings/Fields/ManageAppFields.react.js
@@ -62,7 +62,7 @@ export const ManageAppFields = ({
                 description={
                   <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', gap: '8px' }}>
                     <span>{parseVersion}</span>
-                    {onEditParseVersion && (
+                    {onEditParseVersion && !isCollaborator && (
                       <button
                         type='button'
                         className={styles.changeActionBtn}
