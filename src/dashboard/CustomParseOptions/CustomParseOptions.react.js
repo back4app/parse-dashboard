bacd8c0080fd4e1f4762d0535b0d37f9ac46229b
sadakchap <aliceprerna@gmail.com>
2026-02-26 16:34:19 +0530

    fix opacity

--- src/dashboard/CustomParseOptions/CustomParseOptions.react.js
+++ src/dashboard/CustomParseOptions/CustomParseOptions.react.js
@@ -237,7 +237,7 @@ class CustomParseOptions extends DashboardView {
             </Fieldset>
           )}
 
-          <div style={!isOwner || !this.state.canChangeCustomParseOptions ? { pointerEvents: 'none', opacity: 0.95 } : {}}>
+          <div style={!isOwner || !this.state.canChangeCustomParseOptions ? { pointerEvents: 'none', opacity: 0.6 } : {}}>
 
           <Fieldset
             legend='Core Configuration'
