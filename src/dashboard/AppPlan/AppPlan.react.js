c436a7e3ba293552f777564e54c2a7d35411117f
charles-ramos <charles.ramos@fatec.sp.gov.br>
2026-06-23 15:16:06 -0300

    fix paddle-gtm value

--- src/dashboard/AppPlan/AppPlan.react.js
+++ src/dashboard/AppPlan/AppPlan.react.js
@@ -215,7 +215,7 @@ class AppPlan extends DashboardView {
         window.dataLayer = window.dataLayer || [];
         window.dataLayer.push({
           event: 'paddle_purchase',
-          value: Number(data.data.totals.total),
+          value: Number(data.data.totals.total) * 100,
           currency: data.data.currency_code,
           transaction_id: data.data.transaction_id
         });
